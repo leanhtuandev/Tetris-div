e29ae7794e71bc61037c8503bdf6954e63a2e6c1
leanhtuandev <leanhtuan.yb29@gmail.com>
2020-10-02 13:19:59 +0700

    add code

---
+++ Tetris not canvas/script.js
@@ -0,0 +1,246 @@
+const GRID = document.querySelector('.grid');
+const SCORE = document.querySelector('#score');
+const BTN = document.querySelector('#start-button');
+const WIDTH = 10;
+const BTN2 = document.querySelector('#play-button');
+const COLOR = ['#FF0000', '#FF9700', '#FFFF00', '#6CFF00', '#00C5FF', '#FF00AE'];
+
+// khởi tạo các brick
+const BRICK_L = [
+    [1, WIDTH + 1, WIDTH * 2 + 1, 2],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH * 2 + 2],
+    [1, WIDTH + 1, WIDTH * 2 + 1, WIDTH * 2],
+    [WIDTH, WIDTH * 2, WIDTH * 2 + 1, WIDTH * 2 + 2]
+];
+const BRICK_Z = [
+    [0, WIDTH, WIDTH + 1, WIDTH * 2 + 1],
+    [WIDTH + 1, WIDTH + 2, WIDTH * 2, WIDTH * 2 + 1],
+    [0, WIDTH, WIDTH + 1, WIDTH * 2 + 1],
+    [WIDTH + 1, WIDTH + 2, WIDTH * 2, WIDTH * 2 + 1]
+];
+
+const BRICK_T = [
+    [1, WIDTH, WIDTH + 1, WIDTH + 2],
+    [1, WIDTH + 1, WIDTH + 2, WIDTH * 2 + 1],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH * 2 + 1],
+    [1, WIDTH, WIDTH + 1, WIDTH * 2 + 1]
+];
+
+const BRICK_O = [
+    [0, 1, WIDTH, WIDTH + 1],
+    [0, 1, WIDTH, WIDTH + 1],
+    [0, 1, WIDTH, WIDTH + 1],
+    [0, 1, WIDTH, WIDTH + 1]
+];
+const BRICK_I = [
+    [1, WIDTH + 1, WIDTH * 2 + 1, WIDTH * 3 + 1],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH + 3],
+    [1, WIDTH + 1, WIDTH * 2 + 1, WIDTH * 3 + 1],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH + 3]
+];
+const THE_BRICK = [BRICK_L, BRICK_Z, BRICK_T, BRICK_O, BRICK_I];
+
+//
+let currentPosition = 4;
+let currentRotation = 0;
+
+//  random mảng chứa brick ngẫu nhiên
+let random = Math.floor(Math.random() * THE_BRICK.length);
+let current = THE_BRICK[random][currentRotation];
+
+// random color brick
+let randomColor = Math.floor(Math.random() * COLOR.length);
+let color = COLOR[randomColor];
+console.log(color);
+
+// lấy 1 mảng các ô vuông
+let boxs = Array.from(document.querySelectorAll('.grid div'));
+
+// vẽ brick
+
+
+function draw() {
+    //
+    current.forEach(index => {
+
+        boxs[currentPosition + index].classList.add('theBox');
+        boxs[currentPosition + index].style.backgroundColor = color;
+
+    })
+}
+draw();
+
+// xoa brick
+
+function undraw() {
+    current.forEach(index => {
+        boxs[currentPosition + index].classList.remove('theBox');
+        boxs[currentPosition + index].style.backgroundColor = '';
+    })
+}
+
+// setInterval
+var time = setInterval(moveDown, 1000);
+
+function control(evt) {
+    if (evt.keyCode === 37) {
+        moveLeft();
+    }
+    if (evt.keyCode === 38) {
+        moveUp();
+    }
+    if (evt.keyCode === 39) {
+        moveRight();
+    }
+    if (evt.keyCode === 40) {
+        moveDown();
+    }
+}
+
+document.addEventListener('keydown', control);
+// function move down
+
+function moveDown() {
+    undraw();
+    currentPosition += WIDTH; // điểm tiếp theo bằng điểm hiện tại + game width
+    draw();
+    freeze();
+}
+
+// freeze function
+
+function freeze() {
+    if (current.some(index => boxs[currentPosition + index + WIDTH].classList.contains('last'))) {
+        // vẽ lại brick khi ở cuối
+        current.forEach(index => boxs[currentPosition + index].classList.add('last'));
+        random = Math.floor(Math.random() * THE_BRICK.length);
+        current = THE_BRICK[random][currentRotation];
+        currentPosition = 4;
+        randomColor = Math.floor(Math.random() * COLOR.length);
+        color = COLOR[randomColor];
+        console.log(color);
+        draw();
+
+    }
+    addScore();
+    speed();
+    gameOver();
+}
+
+function moveLeft() {
+    undraw();
+    // kiểm tra brick có ở vị trí đầu tiên trong col hay không
+
+    let isLeft = current.some(index => (currentPosition + index) % WIDTH === 0);
+
+    if (!isLeft) currentPosition -= 1;
+    // kiem tra ben trai co brick k
+    if (current.some(index => boxs[currentPosition + index].classList.contains('last'))) {
+        currentPosition += 1;
+    }
+    draw();
+
+}
+
+function moveRight() {
+    undraw();
+    // kiểm tra brick có ở cuoi cung trong col hay không
+
+    let isRight = current.some(index => (currentPosition + index) % WIDTH === WIDTH - 1);
+
+    if (!isRight) currentPosition += 1;
+    // kiem tra ben phai co brick k
+    if (current.some(index => boxs[currentPosition + index].classList.contains('last'))) {
+        currentPosition -= 1;
+    }
+    draw();
+
+}
+
+function moveUp() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === 4) {
+        currentRotation = 0;
+    }
+    current = THE_BRICK[random][currentRotation];
+    draw();
+}
+
+// start or pause game
+BTN.addEventListener('click', () => {
+    if (time) {
+        clearInterval(time);
+        time = null;
+    } else {
+        time = setInterval(moveDown, 1000);
+    }
+})
+
+//reload game
+
+BTN2.addEventListener('click', () => {
+    location.reload();
+})
+
+// add score 
+var score = 0;
+//loi
+function addScore() {
+    for (let i = 0; i < 199; i += WIDTH) {
+        const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9];
+
+        if (row.every(index => boxs[index].classList.contains('last'))) {
+            score += 10;
+            SCORE.innerHTML = score;
+
+            row.forEach(index => {
+                boxs[index].classList.remove('last');
+                boxs[index].classList.remove('theBox');
+                boxs[index].style.backgroundColor = '';
+            });
+            const squaresRemoved = boxs.splice(i, 10)
+            boxs = squaresRemoved.concat(boxs)
+            boxs.forEach(cell => {
+                GRID.appendChild(cell)
+                cell.style.marginLeft = '1.8px';
+                cell.style.marginRight = '1.8px';
+            })
+        }
+    }
+}
+
+// game over
+
+function gameOver() {
+    if (current.some(index => boxs[currentPosition + index].classList.contains('last'))) {
+        SCORE.innerHTML = 'NGU';
+        console.log('game over')
+        clearInterval(time);
+    }
+}
+
+// Speed
+
+function speed() {
+    if (score >= 100) {
+        clearInterval(time);
+        time = setInterval(moveDown, 800);
+        console.log('800mls')
+    }
+    if (score >= 200) {
+        clearInterval(time);
+        time = setInterval(moveDown, 600);
+        console.log('600mls')
+    }
+    if (score >= 400) {
+        clearInterval(time);
+        time = setInterval(moveDown, 400);
+        console.log('400mls')
+    }
+    if (score >= 800) {
+        clearInterval(time);
+        time = setInterval(moveDown, 200);
+        console.log('200mls')
+    }
+}
